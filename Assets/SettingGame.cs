8c25eef215fdd3e102ff1d6847cbe37859b43f1c
Tronglich262 <lichtrong262@gmail.com>
2025-09-15 17:13:57 +0700

    Update

--- Assets/SettingGame.cs
+++ Assets/SettingGame.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,13 +8,23 @@ public class SettingGame : MonoBehaviour
    public GameObject menu;
    public GameObject setting;
    public GameObject help;
+    public TextMeshProUGUI fpsText;
+    private float deltaTime;
 
-   private void Update()
-   {
-      
-   }
+    private void Start()
+    {
+        Application.targetFrameRate = 240;
+        QualitySettings.vSyncCount = 0; // Tắt V-Sync để FPS limit có hiệu lực
+    }
+    private void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
 
-   public void MenuGame()
+        float fps = 1.0f / deltaTime;
+        fpsText.text = $"FPS: {Mathf.Ceil(fps)}";
+    }
+
+    public void MenuGame()
    {
       menu.SetActive(!menu.activeSelf);
    }
